55e4bb9213233d7525c69db94f5a335069fc0b1c
Andy Song <andy400400@yahoo.com.tw>
2019-07-22 15:19:19 +0800

    資料存入mySQL

--- PTTCrawler/PTTCrawler.py
+++ PTTCrawler/PTTCrawler.py
@@ -3,10 +3,9 @@
 import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
-import pymssql
+import pymysql
 import time
 import datetime
-
 
 class PTTCrawler():
     
@@ -35,40 +34,39 @@ class PTTCrawler():
         
         #所有明細資料
         all_push_data = pd.DataFrame()
-        all_title_data = pd.DataFrame()
-        #title資料
-        subUrl = []
-        title = []
-        author = []
-        date_array = []
-        last_push_time = []
-        update_time = []
-        #內文統計
-        push = []
-        arrow = []
-        boo = []
+        all_article_data = pd.DataFrame()
         
         while day_in_range == True:
             
-            #網頁擷取
+            #title資料
+            subUrl = []
+            title = []
+            author = []
+            date_array = []
+            last_push_time = []
+            update_time = []
+            #內文統計
+            push = []
+            arrow = []
+            boo = []
+
+            #網頁擷取-文章列表
             soup = self.get_html(previous_page)
             previous_page = soup.select("div.btn-group-paging a")[1]["href"].strip('/bbs/Stock/').strip('.html')
             sel = soup.select(".r-ent") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
             
-            #逐筆取得文章資訊
+            #網頁擷取-逐筆取得文章內容
             for x in reversed(sel):
                         
-                #若為公告不抓取資料
-                article_title = x.select('div.title')[0].text.strip('\n')
-                if article_title.find('[公告]') > -1:
+                #判斷是否跳過該篇文章
+                detect_list = self.detect_article(x, subUrl)
+                
+                if(detect_list[0] == ''):
                     continue
-                
-                #若取不到文章連結代表為刪除文章
-                try :
-                    article_id = x.select('div.title a')[0]["href"].strip('/bbs/Stock/').strip('.html')
-                except IndexError:
-                    continue    
-                
+                else:
+                    article_id = detect_list[0]
+                    article_title = detect_list[1]
+
                 #get文章內容
                 soup = self.get_html(article_id)
                 
@@ -88,16 +86,17 @@ class PTTCrawler():
                 elif (count == 1) & (article_date < self.day_end):
                     break
                 
+                #解析推文
                 push_data = self.parse(soup, article_id)
                 
                 #處除重複資料(依文章、tag、使用者)
                 unique_data = push_data.loc[:,'url':'userId'].drop_duplicates()
                 
-                #如果沒有任何推文，放上即時時間
+                #如果沒有任何推文，放上''
                 try :
                     last_push_time.append(push_data['push_time'].values[-1])
                 except:
-                    last_push_time.append(datetime.datetime.now().strftime("%m/%d %H:%M"))
+                    last_push_time.append('')
                 
                 #放入標題(title、author、date)，推文統計
                 subUrl.append(article_id)
@@ -109,18 +108,42 @@ class PTTCrawler():
                 arrow.append(len(unique_data[unique_data.tag=='→']))
                 boo.append(len(unique_data[unique_data.tag=='噓']))
                 
-                #組dataframe
-                data = pd.DataFrame({'url':subUrl, 'date':date_array, 'title':title, 'push':push, 'arrow':arrow, 'boo':boo, 'last_push_time':last_push_time, 'update_time':update_time})
-                
-                #加入
+                 #統整明細資料
                 all_push_data = all_push_data.append(push_data)
-                all_title_data = all_title_data.append(data)
-        
+                
+            #統整主檔資料
+            article_data = pd.DataFrame({'url':subUrl, 'date':date_array, 'title':title, 'push':push, 'arrow':arrow, 'boo':boo, 'last_push_time':last_push_time, 'update_time':update_time})
+            all_article_data = all_article_data.append(article_data)
+            
             count += 1
-        
+                
+        #刪除資料
+        self.delete_db()
+                
         #寫入資料庫
-        self.insert_db(data, all_push_data)
-
+        self.insert_db(all_article_data, all_push_data)
+        
+    #判斷是否跳過該篇文章
+    def detect_article(self, cell, subUrl):
+        error_return = ['','']
+        
+        #若為公告不抓取資料
+        article_title = cell.select('div.title')[0].text.strip('\n')
+        if article_title.find('[公告]') > -1:
+            return error_return
+        
+        #若取不到文章連結代表為刪除文章
+        try :
+            article_id = cell.select('div.title a')[0]["href"].strip('/bbs/Stock/').strip('.html')
+        except IndexError:
+            return error_return
+        
+         #若文章已存在，代表有重複文章(置底文)
+        if(article_id in subUrl):
+            return error_return
+        
+        return (article_id, article_title)
+        
     #get文章內容
     def get_html(self, page_id):
         title_url = "https://www.ptt.cc/bbs/Stock/" + page_id + ".html"   
@@ -128,6 +151,7 @@ class PTTCrawler():
         soup = BeautifulSoup(html.text,"html.parser") #將網頁資料以html.parser
         return soup
     
+    #解析推文
     def parse(self, soup, article_id):
         #清空變數計算文章內訊息變數
         url = []
@@ -150,32 +174,63 @@ class PTTCrawler():
             push_content.append(sel_push_content[index].text.strip().strip(':'))
             push_time.append(sel_push_time[index].text.strip('\n').strip())
     
-        #計算推文數
         push_data = pd.DataFrame({'url':url, 'tag':tag, 'userId':userId, 'push_content':push_content, 'push_time':push_time})
         return push_data
     
+    def get_conn(self):
+        sql_conn =  pymysql.connect(host='localhost',
+                                     port=3306,
+                                     user='root',
+                                     password='1qaz2wsx',
+                                     db='crawler',
+                                     charset='utf8')
+        return sql_conn
+    
+    def delete_db(self):
+        sql_conn = self.get_conn()
+        
+        try :
+             #先刪除明細資料
+            self.delete_push_data(sql_conn)
+            #刪除主檔資料
+            self.delete_article_data(sql_conn)
+        finally:
+            sql_conn.close()
+          
+    #刪除明細資料
+    def delete_push_data(self, sql_conn):    
+        try :
+            cursor = sql_conn.cursor()
+            cursor.execute('delete from crawler.stock_push where url in ( select url from crawler.stock_article where date >= %s )', self.day_end) 
+            sql_conn.commit()
+        except Exception as e:
+            print(e)
+    
+    #刪除主檔資料
+    def delete_article_data(self, sql_conn):    
+        try :
+            cursor = sql_conn.cursor()
+            cursor.execute('delete from crawler.stock_push where date >= %s ', self.day_end) 
+            sql_conn.commit()
+        except Exception as e:
+            print(e)
     
     #寫入資料庫
     def insert_db(self, data, all_push_data):
-        sql_conn = pymssql.connect(
-          server='',
-          user='',
-          password='',
-          database=''
-        )     
+        sql_conn = self.get_conn()
+   
         try :
-            self.insert_title_data(sql_conn, data)
-            self.insert_push_data(sql_conn, all_push_data)
-            
+            self.insert_article_data(sql_conn, data)
+            self.insert_push_data(sql_conn, all_push_data)          
         finally:
             sql_conn.close()
     
     #寫入主檔資料
-    def insert_title_data(self, sql_conn, data):    
+    def insert_article_data(self, sql_conn, data):    
         for index,row in data.iterrows():
             try :
                 cursor = sql_conn.cursor()
-                cursor.execute('INSERT INTO [TEST].[dbo].[ptt_title] ([url],[title],[date],[push],[arrow],[boo],[last_push_time],[update_time])values (%s, %s, %s, %s, %s, %s, %s, %s)'
+                cursor.execute('INSERT INTO crawler.stock_article(url,title,date,push,arrow,boo,last_push_time,update_time)VALUES(%s,%s,%s,%s,%s,%s,%s,%s)'
                                , (row['url'], row['title'], row['date'], row['push'], row['arrow'], row['boo'], row['last_push_time'], row['update_time'])) 
                 sql_conn.commit()
             except Exception as e:
@@ -186,8 +241,8 @@ class PTTCrawler():
         for index,row in all_push_data.iterrows():
             try :
                 cursor = sql_conn.cursor()
-                cursor.execute('INSERT INTO  [TEST].[dbo].[ptt_article] ([url],[tag],[user_id],[msg])values (%s, %s, %s, %s)'
-                               , (row['url'], row['tag'], row['userId'], row['push_content'])) 
+                cursor.execute('INSERT INTO crawler.stock_push (url,tag,user_id,msg,push_time)VALUES(%s, %s, %s, %s, %s)'
+                               , (row['url'], row['tag'], row['userId'], row['push_content'], row['push_time']))
                 sql_conn.commit()
             except Exception as e:
                 print(e)
